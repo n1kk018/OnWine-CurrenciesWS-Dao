91d3307f1a4e1af06ae24a9ec952303255fc3384
n1kk018 <nicolastorero@gmail.com>
2016-10-18 13:26:27 +0200

    fixtures

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `Currency` (`id`,`name`,`code`,`rate`,`created_at`,`updated_at`) VALUES (1,'dollar','USD',1, NOW(),NOW());
+INSERT INTO `Currency` (`id`,`name`,`code`,`rate`,`created_at`,`updated_at`) VALUES (2,'livre sterling','GBP',1.2, NOW(),NOW());
+INSERT INTO `Currency` (`id`,`name`,`code`,`rate`,`created_at`,`updated_at`) VALUES (3,'euro','EUR',1.1, NOW(),NOW());
